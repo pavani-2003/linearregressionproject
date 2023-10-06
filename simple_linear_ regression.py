b98905fb209aa5d4fde4fcc3b0b3a66492cc745f
pavani-2003 <112242637+pavani-2003@users.noreply.github.com>
2023-10-06 13:14:42 +0530

    Update simple_linear_ regression.py

--- simple_linear_ regression.py
+++ simple_linear_ regression.py
@@ -1,5 +1,4 @@
-Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 # Import necessary libraries
 import pandas as pd
 import numpy as np
